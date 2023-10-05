648b29f4ec0e31573ce329356f96c56b9aed33c2
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2023-10-05 15:23:28 -0700

    Added memoization to grid travellers

--- Dynamic_Programming/Grid_Traveller.py
+++ Dynamic_Programming/Grid_Traveller.py
@@ -1,13 +1,15 @@
 from collections import defaultdict
 class Solution:
-    def traveller(self,row,column,path=""):
+    def traveller(self,row,column, memo={}):
+        key = row,",",column
+        if key in memo:
+            return memo[key]
         if row == 1 and column ==1:
-            print("right path = ",path)
             return 1
         if row==0 or column ==0 :
-            print("wrong path =",path)
             return 0
-        return self.traveller(row-1,column,path+" down ") + self.traveller(row,column-1,path+" right ")
+        memo[key] = self.traveller(row-1,column, memo) + self.traveller(row,column-1,memo)
+        return memo[key]
 
 s= Solution()
 print(s.traveller(2,3))
