fcc0693b8ae0270789c2166d977c40a4501babf1
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2023-10-10 16:55:24 -0700

    Added canSum problem

--- Recursion/canSum.py
+++ Recursion/canSum.py
@@ -1,16 +1,22 @@
 class Solution:
     def canSum(self,targetSum, numbers):
-        if targetSum ==0:
-            return True
-        if targetSum <0:
-            return False
-        
-        for i in numbers:
-            newtarget = targetSum - i
-            if(self.canSum(newtarget,numbers) == True):
-                return True
-        
-        return False
+        ans= []
+        help = []
+        def helper(targetSum, numbers,ans,help):
+            if targetSum == 0:
+                ans.append(help)
+                help=[]
+                return ans
+            if targetSum < 0:
+                help=[]
+                return ans
+            for i in numbers:
+                nextsum = targetSum - i
+                help.append(i)
+                helper(nextsum,numbers,ans,help)
+            return ans
+        return helper(targetSum, numbers,ans,help)
+
 
 s = Solution()
 print(s.canSum(7,[5,3,4,7]))
